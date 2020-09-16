9960bc692da0a92df9fccceba5125965f821148f
jfigo <jfigueroa@miumg.edu.gt>
2020-09-16 16:30:22 -0600

    Update login.js
    notificacion en el login.js

--- assets/js/local/login.js
+++ assets/js/local/login.js
@@ -2,34 +2,37 @@ var UrlApi = "http://localhost:53207/API/" // cambiar puerto de nuestra API
 
 
 function InicioDeSesion() {
-var settings = {
-    "url": UrlApi + "InicioDeSesion",
-    "method": "POST",
-    "timeout": 0,
-    "headers": {
-      "Content-Type": "application/json"
-    },
-    "data": JSON.stringify({
-        "TxtEmail": $("#TxtEmail").val(),
-        "TxtPassword": $("#TxtPassword").val()
-    }),
-  };
-  
-  $.ajax(settings).done(function (response) {
+    var settings = {
+        "url": UrlApi + "InicioDeSesion",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify({
+            "TxtEmail": $("#TxtEmail").val(),
+            "TxtPassword": $("#TxtPassword").val()
+        }),
+    };
 
-    $.each(response, function(index, data) {
-        if(data.IntResultado > 0){
-            sessionStorage.setItem('token',data.TxtToken);
-            var token = sessionStorage.getItem('token');
-            alert("Valor en variable de sesion es: " + token);
-            window.location.href="./src/dashboard.php";
-        }else{
-            var alerta = document.getElementById("alerta");
-            alerta.classList.remove("d-none");
-            alerta.innerHTML = "<strong>Oops! </strong>" + data.TxtToken + "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>"
-            +"<span aria-hidden='true'>&times;</span></button>";
-        }
+    $.ajax(settings).done(function(response) {
+
+        $.each(response, function(index, data) {
+            if (data.IntResultado > 0) {
+                sessionStorage.setItem('token', data.TxtToken);
+                var token = sessionStorage.getItem('token');
+                setTimeout(function() {
+                    myNotification.showNotification('fas fa-smile', 'info', 'Bienvenido !', 'Token: ' + token);
+                }, 2000);
+                //alert("Valor en variable de sesion es: " + token);
+                window.location.href = "./src/dashboard.php";
+            } else {
+                /*var alerta = document.getElementById("alerta");
+                alerta.classList.remove("d-none");
+                alerta.innerHTML = "<strong>Oops! </strong>" + data.TxtToken + "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>"
+                +"<span aria-hidden='true'>&times;</span></button>";*/
+                myNotification.showNotification('fas fa-heart-broken', 'danger', 'OOOPS !', 'Usuario o COntraseña invalida.');
+            }
+        });
     });
-  });
 }
- 
