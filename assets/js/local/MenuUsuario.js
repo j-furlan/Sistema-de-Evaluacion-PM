06f7c353ad422f2f13d51d38b566c820c9815dfe
jfurlano <61610110+jfurlano@users.noreply.github.com>
2020-09-17 00:20:07 -0600

    Implementacion de MenuUsuario

--- assets/js/local/MenuUsuario.js
+++ assets/js/local/MenuUsuario.js
@@ -1,4 +1,4 @@
-var UrlApi = "http://localhost:53207/API/" // cambiar puerto de nuestra API
+var UrlApi = "http://localhost:64315/API/" // cambiar puerto de nuestra API
 
 function MenuUsuario() {
     var settings = {
@@ -21,25 +21,23 @@ function MenuUsuario() {
             var IdMenuPadre = data.IdMenuPadre;
 
             if (IdMenuPadre == 0) {
-                var collapseId = "collapse"+data.TxtNombre;
-                var collapseIdTarget = "#collapse"+data.TxtNombre;
+                var collapseId = "collapse"+ data.IdMenu;
+                var collapseIdTarget = "#collapse" + data.IdMenu;
                 var opcion =
                     "<li class='nav-item'>" +
-                    "<a class='nav-link collapsed' href='' data-toggle='collapse' data-target='" + collapseIdTarget + "' >" +
+                    "<a class='nav-link collapsed' href='#' data-toggle='collapse' data-target='" + collapseIdTarget + "' >" +
                     "<i class='fas fa-fw fa-cog'></i>" +
                     "<span>" + data.TxtNombre + "</span>" +
                     "</a>" +
                     "<div class='collapse' id='" + collapseId + "' data-parent='#accordionSidebar'>" +
-                    "<div class='bg-white py-2 collapse-inner rounded'>";                    
+                    "<div class='bg-white py-2 collapse-inner rounded' id='"+data.IdMenu+"'>"+
+                    "</div></div></li>";                    
             } else {
-                var item = "<a class='collapse-item' href='" + data.TxtLink + ".html'>" + data.TxtNombre + "</a>" +
-                    "</div>" +
-                    "</div>" +
-                    "</li>";
+                var item = "<a class='collapse-item' href='" + data.TxtLink + ".php'>" + data.TxtNombre + "</a>";
             }
 
-            $(opcion).appendTo(".MenuUsuario");
-            $(item).appendTo("#" + data.IdMenuPadre);
+            $(opcion).appendTo(".MenuUsuario");            
+            $(item).appendTo("#" + IdMenuPadre);
 
         });
 
