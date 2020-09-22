12518c80114cd1256b8318b86984820a91e1f2d9
jfigo <jfigueroa@miumg.edu.gt>
2020-09-22 10:56:36 -0600

    Update usuarios.php

--- src/usuarios.php
+++ src/usuarios.php
@@ -153,8 +153,7 @@
           </form>
         </div>
         <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="#">Logout</a>
+          
         </div>
       </div>
     </div>
