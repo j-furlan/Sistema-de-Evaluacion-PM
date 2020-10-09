69981898d967c451c7342d0cfb48f07210204987
jfurlano <61610110+jfurlano@users.noreply.github.com>
2020-10-08 22:21:10 -0600

    Update MenuUsuario.js
    Se modificó la funcion MenuUsuarioLocal que ahora obtiene los datos para el menú desde la variable de sesión.

--- assets/js/local/MenuUsuario.js
+++ assets/js/local/MenuUsuario.js
@@ -1,60 +1,34 @@
-var UrlApi = "http://localhost:53207/API/" // cambiar puerto de nuestra API
+var UrlApi = "http://localhost:64315/API/" // cambiar puerto de nuestra API
 
-function MenuUsuario() {
-    var settings = {
-        "url": UrlApi + "MenuUsuario",
-        "method": "POST",
-        "timeout": 0,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "data": JSON.stringify({
-            "TxtToken": sessionStorage.getItem('token'),
-            "IdModulo": 1
-        }),
-    };
-    //console.log(sessionStorage.getItem('token'));
+//OBTIENE LOS DATOS PARA EL MENU DESDE LA VARIABLE DE SESION.
 
-    $.ajax(settings).done(function(response) {
-//console.log(response);
-if(response.length == 0){
-    setTimeout(function() {
-        myNotification.showNotification('fas fa-heart-broken', 'danger', 'Ooopps!', 'La Sesion ha conlcuido.');
-    }, 2000);
-    window.location.href = "../index.html";
+function MenuUsuarioLocal() {
 
-}
-else{
-    //alert("este es el menu");
-//let ResultadoMenu = response;
-sessionStorage.setItem('ResultadoMenuDeUsuario', JSON.stringify(response));
+    let MenuDeUsuario = JSON.parse(sessionStorage.getItem('ResultadoMenuDeUsuario'));
+    console.log(MenuDeUsuario);
+    if (MenuDeUsuario !== "") {
+        $.each(MenuDeUsuario, function (index, data) {
+            var IdMenuPadre = data.IdMenuPadre;
 
-//console.log(response);
-/* $.each(response, function(index, data) {
-    var IdMenuPadre = data.IdMenuPadre;
+            if (IdMenuPadre == 0) {
+                var collapseId = "collapse" + data.IdMenu;
+                var collapseIdTarget = "#collapse" + data.IdMenu;
+                var opcion =
+                    "<li class='nav-item'>" +
+                    "<a class='nav-link collapsed' href='#' data-toggle='collapse' data-target='" + collapseIdTarget + "' >" +
+                    "<i class='fas fa-fw fa-cog'></i>" +
+                    "<span>" + data.TxtNombre + "</span>" +
+                    "</a>" +
+                    "<div class='collapse' id='" + collapseId + "' data-parent='#accordionSidebar'>" +
+                    "<div class='bg-white py-2 collapse-inner rounded' id='" + data.IdMenu + "'>" +
+                    "</div></div></li>";
+            } else {
+                var item = "<a class='collapse-item' href='" + data.TxtLink + ".php'>" + data.TxtNombre + "</a>";
+            }
+            //console.log(item);
+            $(opcion).appendTo(".MenuUsuario");
+            $(item).appendTo("#" + IdMenuPadre);
 
-    if (IdMenuPadre == 0) {
-        var collapseId = "collapse"+ data.IdMenu;
-        var collapseIdTarget = "#collapse" + data.IdMenu;
-        var opcion =
-            "<li class='nav-item'>" +
-            "<a class='nav-link collapsed' href='#' data-toggle='collapse' data-target='" + collapseIdTarget + "' >" +
-            "<i class='fas fa-fw fa-cog'></i>" +
-            "<span>" + data.TxtNombre + "</span>" +
-            "</a>" +
-            "<div class='collapse' id='" + collapseId + "' data-parent='#accordionSidebar'>" +
-            "<div class='bg-white py-2 collapse-inner rounded' id='"+data.IdMenu+"'>"+
-            "</div></div></li>";                    
-    } else {
-        var item = "<a class='collapse-item' href='" + data.TxtLink + ".php'>" + data.TxtNombre + "</a>";
+        });
     }
-//console.log(item);
-    $(opcion).appendTo(".MenuUsuario");            
-    $(item).appendTo("#" + IdMenuPadre);
-
-}); */
-    
-
-    }
-});
 }
